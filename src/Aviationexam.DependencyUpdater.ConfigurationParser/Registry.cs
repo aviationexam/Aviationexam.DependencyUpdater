0763700347db4a467c235c52290525a48dd1566f
Jan Trejbal <jan@trejbal.land>
2025-04-28 15:09:46 +0200

    Bump Aviationexam.DependencyUpdater.ConfigurationParser

---
+++ src/Aviationexam.DependencyUpdater.ConfigurationParser/Registry.cs
@@ -0,0 +1,8 @@
+using Corvus.Json;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Aviationexam.DependencyUpdater.ConfigurationParser;
+
+[JsonSchemaTypeGenerator("dependabot-2.0.json#/definitions/registry")]
+[SuppressMessage("Design", "MA0077:A class that provides Equals(T) should implement IEquatable<T>")]
+public readonly partial struct Registry;
