b37b7a3d8c9ef5795d89b1447460c350d5999996
Jan Trejbal <jan@trejbal.land>
2025-04-29 22:31:25 +0200

    Update pipeline

--- src/Aviationexam.DependencyUpdater/Program.cs
+++ src/Aviationexam.DependencyUpdater/Program.cs
@@ -1,10 +1,13 @@
 using Aviationexam.DependencyUpdater.ConfigurationParser;
 using Aviationexam.DependencyUpdater.DefaultImplementations;
+using Aviationexam.DependencyUpdater.Interfaces;
 using Aviationexam.DependencyUpdater.Nuget;
+using Corvus.Json;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -25,21 +28,51 @@ builder.Services.AddDefaultImplementations();
 
 using var host = builder.Build();
 
+var dependabotConfigurationLoader = host.Services.GetRequiredService<DependabotConfigurationLoader>();
+var envVariableProvider = host.Services.GetRequiredService<IEnvVariableProvider>();
 var nugetUpdater = host.Services.GetRequiredService<NugetUpdater>();
 var nugetVersionFetcherFactory = host.Services.GetRequiredService<NugetVersionFetcherFactory>();
 var logger = host.Services.GetRequiredService<ILogger<NugetUpdater>>();
 
-var directoryPath = "/opt/asp.net/AviationexamWebV3/Src-V3";
-var nugetUpdaterContext = nugetUpdater.CreateContext(
-    directoryPath
-);
+var directoryPath = "/opt/asp.net/AviationexamWebV3/";
+var dependabotConfigurations = dependabotConfigurationLoader.LoadConfiguration(directoryPath);
 
-var sourceRepositories = nugetUpdaterContext.NugetConfigurations.ToDictionary(
-    x => x,
-    nugetVersionFetcherFactory.CreateSourceRepository
-);
-
-var dependencies = nugetUpdaterContext.MapSourceToDependency(logger);
-foreach (var (dependency, sources) in dependencies)
+foreach (var dependabotConfiguration in dependabotConfigurations)
 {
+    var nugetUpdates = dependabotConfiguration.ExtractEcosystemUpdates("nuget");
+
+    if (nugetUpdates.Count == 0)
+    {
+        continue;
+    }
+
+    var nugetFeedAuthentications = dependabotConfiguration.ExtractFeeds(
+        "nuget-feed",
+        x => NugetFeedAuthenticationFactory.CreateNugetFeedAuthentication(
+            envVariableProvider,
+            x.Url.GetString()!,
+            x.Username.GetString(),
+            x.Password.GetString(),
+            x.Token.GetString()
+        )
+    );
+
+    foreach (var nugetUpdate in nugetUpdates)
+    {
+        var directory = nugetUpdate.DirectoryValue.GetString();
+
+        var nugetUpdaterContext = nugetUpdater.CreateContext(
+            Path.Join(directoryPath, directory)
+        );
+
+        var sourceRepositories = nugetUpdaterContext.NugetConfigurations.ToDictionary(
+            x => x,
+            x => nugetVersionFetcherFactory.CreateSourceRepository(x, nugetFeedAuthentications)
+        );
+
+        var dependencies = nugetUpdaterContext.MapSourceToDependency(logger);
+        foreach (var (dependency, sources) in dependencies)
+        {
+        }
+    }
 }
