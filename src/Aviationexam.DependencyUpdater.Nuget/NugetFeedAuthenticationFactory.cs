406f7aad25e7eaf74d9ea714e972267ff66f6c89
Jan Trejbal <jan@trejbal.land>
2025-04-29 22:44:43 +0200

    Implement CreateNugetFeedAuthentication

--- src/Aviationexam.DependencyUpdater.Nuget/NugetFeedAuthenticationFactory.cs
+++ src/Aviationexam.DependencyUpdater.Nuget/NugetFeedAuthenticationFactory.cs
@@ -1,4 +1,6 @@
+using Aviationexam.DependencyUpdater.Common;
 using Aviationexam.DependencyUpdater.Interfaces;
+using System;
 
 namespace Aviationexam.DependencyUpdater.Nuget;
 
@@ -12,7 +14,25 @@ public static class NugetFeedAuthenticationFactory
         string? token
     )
     {
-        // TODO envVariableProvider, token
-        return new NugetFeedAuthentication(url, username, password);
+        username = username is null ? null : envVariableProvider.PopulateEnvironmentVariables(username);
+        password = password is null ? null : envVariableProvider.PopulateEnvironmentVariables(password);
+        token = token is null ? null : envVariableProvider.PopulateEnvironmentVariables(token);
+
+        if (
+            string.IsNullOrEmpty(username)
+            && string.IsNullOrEmpty(password)
+            && !string.IsNullOrEmpty(token)
+            && token.Split(':', 2, StringSplitOptions.TrimEntries) is { Length: 2 } explodedToken
+        )
+        {
+            username = explodedToken[0];
+            password = explodedToken[1];
+        }
+
+        return new NugetFeedAuthentication(
+            url,
+            username,
+            password
+        );
     }
 }
