d673d2ce2691697f66581d984e4da3001f552380
Jan Trejbal <jan@trejbal.land>
2025-04-30 15:52:02 +0200

    Drop unused NugetDependencyJsonSerializerContext

--- src/Aviationexam.DependencyUpdater.Nuget/NugetDependencyJsonSerializerContext.cs
+++
@@ -1,6 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace Aviationexam.DependencyUpdater.Nuget;
-
-[JsonSerializable(typeof(NugetDependency))]
-internal sealed partial class NugetDependencyJsonSerializerContext : JsonSerializerContext;
