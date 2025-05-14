e3e214c51f24e3c379c0b0fa4a16707e2b541073
Jan Trejbal <jan@trejbal.land>
2025-05-14 16:57:51 +0200

    Add IsCompatibleWithCurrentVersions

--- src/Aviationexam.DependencyUpdater.Nuget/NugetVersionWriter.cs
+++ src/Aviationexam.DependencyUpdater.Nuget/NugetVersionWriter.cs
@@ -1,5 +1,7 @@
 using Aviationexam.DependencyUpdater.Common;
 using Aviationexam.DependencyUpdater.Interfaces;
+using NuGet.Protocol;
+using System;
 using System.Collections.Generic;
 
 namespace Aviationexam.DependencyUpdater.Nuget;
@@ -12,10 +14,52 @@ public sealed class NugetVersionWriter
         IDictionary<string, PackageVersion> groupPackageVersions
     )
     {
+        if (!IsCompatibleWithCurrentVersions(nugetUpdateCandidate.PackageVersion, groupPackageVersions, out _))
+        {
+            return false;
+        }
+
         var workspaceDirectory = gitWorkspace.GetWorkspaceDirectory();
 
         var targetFullPath = nugetUpdateCandidate.NugetDependency.NugetFile.GetFullPath(workspaceDirectory);
 
         return true;
+    }
+
+    public bool IsCompatibleWithCurrentVersions<T>(
+        PackageVersion<T> packageVersion,
+        IDictionary<string, PackageVersion> groupPackageVersions,
+        out Package? conflictingPackageVersion
+    )
+    {
+        if (packageVersion is PackageVersion<PackageSearchMetadataRegistration> packageSearchMetadataRegistration)
+        {
+            foreach (var dependencySet in packageSearchMetadataRegistration.OriginalReference.DependencySets)
+            {
+                foreach (var dependencyPackage in dependencySet.Packages)
+                {
+                    if (groupPackageVersions.TryGetValue(dependencyPackage.Id, out var dependencyCurrentVersion))
+                    {
+                        if (
+                            dependencyPackage.VersionRange.MinVersion is { } dependencyPackageMinVersion
+                            && dependencyPackageMinVersion.MapToPackageVersion() is { } dependencyPackageVersion
+                            && dependencyPackageVersion < dependencyCurrentVersion
+                        )
+                        {
+                            conflictingPackageVersion = new Package(
+                                dependencyPackage.Id,
+                                dependencyPackageVersion
+                            );
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            conflictingPackageVersion = null;
+            return true;
+        }
+
+        throw new ArgumentOutOfRangeException(nameof(packageVersion), packageVersion, null);
     }
 }
