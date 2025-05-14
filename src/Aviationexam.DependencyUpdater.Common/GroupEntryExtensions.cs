d16629dfed8b72c548518e7dfaa96bda7588ecbb
Jan Trejbal <jan@trejbal.land>
2025-05-14 20:53:02 +0200

    Fix GetBranchName

--- src/Aviationexam.DependencyUpdater.Common/GroupEntryExtensions.cs
+++ src/Aviationexam.DependencyUpdater.Common/GroupEntryExtensions.cs
@@ -5,6 +5,6 @@ public static class GroupEntryExtensions
     public static string GetBranchName(
         this GroupEntry groupEntry
     ) => groupEntry.Patterns.Count == 0
-        ? $"dependency-updater/group-{groupEntry.GroupName}"
-        : $"dependency-updater/package-{groupEntry.GroupName}";
+        ? $"dependency-updater/package-{groupEntry.GroupName}"
+        : $"dependency-updater/group-{groupEntry.GroupName}";
 }
