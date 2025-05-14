b27e5a49f8aed1078579d83a349851fad52f63e2
Jan Trejbal <jan@trejbal.land>
2025-05-14 19:30:33 +0200

    Fix GitSourceVersioningFactory

--- src/Aviationexam.DependencyUpdater.Vcs.Git/GitSourceVersioningFactory.cs
+++ src/Aviationexam.DependencyUpdater.Vcs.Git/GitSourceVersioningFactory.cs
@@ -1,15 +1,19 @@
 using Aviationexam.DependencyUpdater.Interfaces;
 using LibGit2Sharp;
+using System;
 
 namespace Aviationexam.DependencyUpdater.Vcs.Git;
 
-public sealed class GitSourceVersioningFactory : ISourceVersioningFactory
+public sealed class GitSourceVersioningFactory(
+    TimeProvider timeProvider
+) : ISourceVersioningFactory
 {
     public ISourceVersioning CreateSourceVersioning(
         string sourceDirectory
     ) => new GitSourceVersioning(
 #pragma warning disable CA2000
-        new Repository(sourceDirectory)
+        new Repository(sourceDirectory),
 #pragma warning restore CA2000
+        timeProvider
     );
 }
