444273e55ee657bbb026b064e8d8decebab0e06c
Jan Trejbal <jan@trejbal.land>
2025-05-15 20:21:58 +0200

    Add OrganizationEndpoint

--- src/Aviationexam.DependencyUpdater.Repository.DevOps/DevOpsConfiguration.cs
+++ src/Aviationexam.DependencyUpdater.Repository.DevOps/DevOpsConfiguration.cs
@@ -1,7 +1,11 @@
+using System;
+
 namespace Aviationexam.DependencyUpdater.Repository.DevOps;
 
 public sealed class DevOpsConfiguration
 {
+    public Uri OrganizationEndpoint => new($"https://dev.azure.com/{Organization}", UriKind.Absolute);
+
     public required string Organization { get; set; }
 
     public required string PersonalAccessToken { get; set; }
