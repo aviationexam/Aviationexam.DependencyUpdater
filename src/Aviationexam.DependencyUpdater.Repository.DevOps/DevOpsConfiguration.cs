4f60fb388cc3e4e493519779e9455895172c0540
Jan Trejbal <jan@trejbal.land>
2025-05-15 20:06:26 +0200

    Change DevOpsConfiguration to class

--- src/Aviationexam.DependencyUpdater.Repository.DevOps/DevOpsConfiguration.cs
+++ src/Aviationexam.DependencyUpdater.Repository.DevOps/DevOpsConfiguration.cs
@@ -1,8 +1,12 @@
 namespace Aviationexam.DependencyUpdater.Repository.DevOps;
 
-public sealed record DevOpsConfiguration(
-    string Organization,
-    string PersonalAccessToken,
-    string Project,
-    string Repository
-);
+public sealed class DevOpsConfiguration
+{
+    public required string Organization { get; set; }
+
+    public required string PersonalAccessToken { get; set; }
+
+    public required string Project { get; set; }
+
+    public required string Repository { get; set; }
+}
