6a564f87efb1f8ce44e7f4d25a2ff970b85bbc86
Jan Trejbal <jan@trejbal.land>
2025-05-20 17:37:59 +0200

    Add EPackageSource into MapToPackageVersion

--- src/Aviationexam.DependencyUpdater.Nuget/NuGetVersionExtensions.cs
+++ src/Aviationexam.DependencyUpdater.Nuget/NuGetVersionExtensions.cs
@@ -6,11 +6,13 @@ namespace Aviationexam.DependencyUpdater.Nuget;
 public static class NuGetVersionExtensions
 {
     public static PackageVersion MapToPackageVersion(
-        this NuGetVersion nuGetVersion
+        this NuGetVersion nuGetVersion,
+        EPackageSource packageSource
     ) => new(
         nuGetVersion.Version,
         nuGetVersion.IsPrerelease,
         [.. nuGetVersion.ReleaseLabels],
+        packageSource,
         NugetReleaseLabelComparer.Instance
     );
 }
--- src/Aviationexam.DependencyUpdater.Nuget/PackageSearchMetadataRegistrationExtensions.cs
+++ src/Aviationexam.DependencyUpdater.Nuget/PackageSearchMetadataRegistrationExtensions.cs
@@ -6,11 +6,13 @@ namespace Aviationexam.DependencyUpdater.Nuget;
 public static class PackageSearchMetadataRegistrationExtensions
 {
     public static PackageVersion<PackageSearchMetadataRegistration> MapToPackageVersion(
-        this PackageSearchMetadataRegistration packageSearchMetadataRegistration
+        this PackageSearchMetadataRegistration packageSearchMetadataRegistration,
+        EPackageSource packageSource
     ) => new(
         packageSearchMetadataRegistration.Version.Version,
         packageSearchMetadataRegistration.Version.IsPrerelease,
         [.. packageSearchMetadataRegistration.Version.ReleaseLabels],
+        packageSource,
         NugetReleaseLabelComparer.Instance,
         packageSearchMetadataRegistration
     );
