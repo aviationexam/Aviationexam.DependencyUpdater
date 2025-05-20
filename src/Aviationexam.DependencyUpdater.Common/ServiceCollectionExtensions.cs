ce0927c07bb797bbeac64874b1b4f56a31aa222d
Jan Trejbal <jan@trejbal.land>
2025-05-20 16:15:16 +0200

    Resolve false-positive formater changes

--- src/Aviationexam.DependencyUpdater.Common/ServiceCollectionExtensions.cs
+++ src/Aviationexam.DependencyUpdater.Common/ServiceCollectionExtensions.cs
@@ -1,10 +1,13 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Aviationexam.DependencyUpdater.Common;
 
 public static class ServiceCollectionExtensions
 {
+    [UnconditionalSuppressMessage("Trimming", "IL2026", Justification = "Safe due to source-generated binding")]
+    [UnconditionalSuppressMessage("AOT", "IL3050", Justification = "Safe due to source-generated binding")]
     public static IServiceCollection AddCommon(
         this IServiceCollection services, IConfigurationRoot configuration
     ) => services
--- src/Aviationexam.DependencyUpdater.Repository.DevOps/ServiceCollectionExtensions.cs
+++ src/Aviationexam.DependencyUpdater.Repository.DevOps/ServiceCollectionExtensions.cs
@@ -1,11 +1,14 @@
 using Aviationexam.DependencyUpdater.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Aviationexam.DependencyUpdater.Repository.DevOps;
 
 public static class ServiceCollectionExtensions
 {
+    [UnconditionalSuppressMessage("Trimming", "IL2026", Justification = "Safe due to source-generated binding")]
+    [UnconditionalSuppressMessage("AOT", "IL3050", Justification = "Safe due to source-generated binding")]
     public static IServiceCollection AddRepositoryDevOps(
         this IServiceCollection services, IConfigurationRoot configuration
     ) => services
