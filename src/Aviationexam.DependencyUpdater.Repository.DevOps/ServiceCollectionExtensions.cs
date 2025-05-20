ed1860ed4a91f8058d43c455d2f6a247729eda38
Jan Trejbal <jan@trejbal.land>
2025-05-20 16:03:40 +0200

    Drop RequiresDynamicCode, RequiresUnreferencedCode

--- src/Aviationexam.DependencyUpdater.Repository.DevOps/ServiceCollectionExtensions.cs
+++ src/Aviationexam.DependencyUpdater.Repository.DevOps/ServiceCollectionExtensions.cs
@@ -1,14 +1,11 @@
 using Aviationexam.DependencyUpdater.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Aviationexam.DependencyUpdater.Repository.DevOps;
 
 public static class ServiceCollectionExtensions
 {
-    [RequiresDynamicCode()]
-    [RequiresUnreferencedCode()]
     public static IServiceCollection AddRepositoryDevOps(
         this IServiceCollection services, IConfigurationRoot configuration
     ) => services
