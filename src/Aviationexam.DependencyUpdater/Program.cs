3e1b5532335c1ee69f80a7b36faf08ca209cf819
Jan Trejbal <jan@trejbal.land>
2025-05-22 17:04:29 +0200

    Add arguments description

--- src/Aviationexam.DependencyUpdater/Program.cs
+++ src/Aviationexam.DependencyUpdater/Program.cs
@@ -2,18 +2,78 @@ using Aviationexam.DependencyUpdater;
 using System.CommandLine;
 using System.IO;
 
-var directory = new Option<string>("--directory", description: "The directory containing the repository to update dependencies in", getDefaultValue: Directory.GetCurrentDirectory)
+var directory = new Option<string>(
+    "--directory",
+    description: "The path to the local Git repository where dependencies will be updated.",
+    getDefaultValue: Directory.GetCurrentDirectory
+)
 {
     IsRequired = true,
 };
-var organization = new Option<string>("--organization") { IsRequired = true };
-var project = new Option<string>("--project") { IsRequired = true };
-var repository = new Option<string>("--repository") { IsRequired = true };
-var pat = new Option<string>("--pat") { IsRequired = true };
-var accountId = new Option<string>("--account-id") { IsRequired = true };
-var nugetFeedId = new Option<string>("--nuget-feed-id") { IsRequired = true };
-var serviceHost = new Option<string>("--nuget-service-host") { IsRequired = true };
-var accessTokenResourceId = new Option<string>("--access-token-resource-id") { IsRequired = true };
+
+var organization = new Option<string>(
+    "--organization",
+    description: "The name of the Azure DevOps organization (e.g., 'contoso')."
+)
+{
+    IsRequired = true,
+};
+
+var project = new Option<string>(
+    "--project",
+    description: "The Azure DevOps project that contains the target repository."
+)
+{
+    IsRequired = true,
+};
+
+var repository = new Option<string>(
+    "--repository",
+    description: "The name of the Azure DevOps Git repository to operate on."
+)
+{
+    IsRequired = true,
+};
+
+var pat = new Option<string>(
+    "--pat",
+    description: "The Azure DevOps personal access token used for authentication."
+)
+{
+    IsRequired = true,
+};
+
+var accountId = new Option<string>(
+    "--account-id",
+    description: "The Azure DevOps user or service account ID used for API access."
+)
+{
+    IsRequired = true,
+};
+
+var nugetFeedId = new Option<string>(
+    "--nuget-feed-id",
+    description: "The ID of the Azure Artifacts NuGet feed used for dependency resolution."
+)
+{
+    IsRequired = true,
+};
+
+var serviceHost = new Option<string>(
+    "--nuget-service-host",
+    description: "The internal Azure DevOps service host identifier used when accessing upstream package data."
+)
+{
+    IsRequired = true,
+};
+
+var accessTokenResourceId = new Option<string>(
+    "--access-token-resource-id",
+    description: "The Azure Active Directory resource ID used to acquire access tokens for upstream ingestion."
+)
+{
+    IsRequired = true,
+};
 
 var rootCommand = new RootCommand("Dependency updater tool that processes dependency updates based on configuration files.")
 {
