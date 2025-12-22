157d7064a29c5d34dd6294f82b934c91fa85bcdc
Jan Trejbal <jan@trejbal.land>
2025-12-22 23:57:54 +0100

    Typo

--- src/Aviationexam.DependencyUpdater.Nuget/Extensions/PackageSearchMetadataRegistrationExtensions.cs
+++ src/Aviationexam.DependencyUpdater.Nuget/Extensions/PackageSearchMetadataRegistrationExtensions.cs
@@ -3,7 +3,6 @@ using Aviationexam.DependencyUpdater.Nuget.Services;
 using NuGet.Packaging;
 using NuGet.Protocol;
 using System.Collections.Generic;
-using System.Linq;
 using ZLinq;
 
 namespace Aviationexam.DependencyUpdater.Nuget.Extensions;
