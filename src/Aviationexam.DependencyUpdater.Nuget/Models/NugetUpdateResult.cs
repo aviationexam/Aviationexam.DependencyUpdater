51f9ab97a55b25f9483f4551541799526f476722
Jan Trejbal <jan@trejbal.land>
2026-01-19 22:27:14 +0100

    Drop NugetUpdateResult

--- src/Aviationexam.DependencyUpdater.Nuget/Models/NugetUpdateResult.cs
+++
@@ -1,9 +0,0 @@
-using Aviationexam.DependencyUpdater.Common;
-using System.Collections.Generic;
-
-namespace Aviationexam.DependencyUpdater.Nuget.Models;
-
-public sealed record NugetUpdateResult(
-    NugetUpdateCandidate UpdateCandidate,
-    PackageVersion FromVersion
-);
